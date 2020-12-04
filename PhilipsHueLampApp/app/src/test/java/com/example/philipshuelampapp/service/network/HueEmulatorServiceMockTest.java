032ed85b897c5a5bd81ed5054e9f714ede6d5a2d
Conner Linskens <cmh.linskens@student.avans.nl>
2020-12-04 21:20:12 +0100

    Started on Mock test

---
+++ PhilipsHueLampApp/app/src/test/java/com/example/philipshuelampapp/service/network/HueEmulatorServiceMockTest.java
@@ -0,0 +1,14 @@
+package com.example.philipshuelampapp.service.network;
+
+import org.junit.jupiter.api.Test;
+
+import okhttp3.mockwebserver.MockWebServer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HueEmulatorServiceMockTest {
+
+    final MockWebServer mockWebServer = new MockWebServer();
+
+
+}
